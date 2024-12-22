8a12b5a8dac721c082772b337c83bad3671e8dfe
Yuheng Su <gipsyh.icu@gmail.com>
2024-12-22 04:31:54 +0000

    v0.2.3

--- src/grc.rs
+++ src/grc.rs
@@ -5,146 +5,93 @@ use std::{
     sync::Arc,
 };
 
-#[derive(Debug, Clone, Default)]
-pub struct Grc<T> {
-    rc: Rc<T>,
-}
-
-impl<T> Grc<T> {
-    #[inline]
-    pub fn new(v: T) -> Self {
-        Self { rc: Rc::new(v) }
-    }
-
-    #[inline]
-    pub fn as_ptr(&self) -> *const T {
-        Rc::as_ptr(&self.rc)
-    }
-
-    #[inline]
-    pub fn from_ptr(p: *const T) -> Self {
-        Self {
-            rc: unsafe { Rc::from_raw(p) },
+macro_rules! define_rc_wrapper {
+    ($wrapper:ident, $inner:ident) => {
+        #[derive(Debug, Default)]
+        pub struct $wrapper<T> {
+            inner: $inner<T>,
         }
-    }
 
-    #[inline]
-    pub fn count(&self) -> usize {
-        Rc::strong_count(&self.rc)
-    }
+        impl<T> $wrapper<T> {
+            #[inline]
+            pub fn new(v: T) -> Self {
+                Self {
+                    inner: $inner::new(v),
+                }
+            }
 
-    #[inline]
-    pub fn increment_count(&self) {
-        unsafe { Rc::increment_strong_count(self.as_ptr()) }
-    }
-}
+            #[inline]
+            pub fn as_ptr(&self) -> *const T {
+                $inner::as_ptr(&self.inner)
+            }
 
-impl<T> Deref for Grc<T> {
-    type Target = T;
+            #[inline]
+            pub fn from_ptr(p: *const T) -> Self {
+                Self {
+                    inner: unsafe { $inner::from_raw(p) },
+                }
+            }
 
-    #[inline]
-    fn deref(&self) -> &Self::Target {
-        self.rc.deref()
-    }
-}
+            #[inline]
+            pub fn count(&self) -> usize {
+                $inner::strong_count(&self.inner)
+            }
 
-impl<T> DerefMut for Grc<T> {
-    #[inline]
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        unsafe { Rc::get_mut_unchecked(&mut self.rc) }
-    }
-}
+            #[inline]
+            pub fn increment_count(&self) {
+                unsafe { $inner::increment_strong_count(self.as_ptr()) }
+            }
 
-impl<T> PartialEq for Grc<T> {
-    #[inline]
-    fn eq(&self, other: &Self) -> bool {
-        Rc::as_ptr(&self.rc) == Rc::as_ptr(&other.rc)
-    }
-}
-
-impl<T> Eq for Grc<T> {}
-
-impl<T> Hash for Grc<T> {
-    #[inline]
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        Rc::as_ptr(&self.rc).hash(state);
-    }
-}
-
-unsafe impl<T> Sync for Grc<T> {}
-
-#[derive(Debug, Default)]
-pub struct Garc<T> {
-    arc: Arc<T>,
-}
-
-impl<T> Garc<T> {
-    #[inline]
-    pub fn new(v: T) -> Self {
-        Self { arc: Arc::new(v) }
-    }
-
-    #[inline]
-    pub fn as_ptr(&self) -> *const T {
-        Arc::as_ptr(&self.arc)
-    }
-
-    #[inline]
-    pub fn from_ptr(p: *const T) -> Self {
-        Self {
-            arc: unsafe { Arc::from_raw(p) },
+            #[inline]
+            pub unsafe fn get_mut_from_unmut(&self) -> &mut T {
+                unsafe { &mut *(self.as_ptr() as *mut T) }
+            }
         }
-    }
 
-    #[inline]
-    pub fn count(&self) -> usize {
-        Arc::strong_count(&self.arc)
-    }
+        impl<T> Deref for $wrapper<T> {
+            type Target = T;
 
-    #[inline]
-    pub fn increment_count(&self) {
-        unsafe { Rc::increment_strong_count(self.as_ptr()) }
-    }
-}
-
-impl<T> Deref for Garc<T> {
-    type Target = T;
-
-    #[inline]
-    fn deref(&self) -> &Self::Target {
-        self.arc.deref()
-    }
-}
-
-impl<T> DerefMut for Garc<T> {
-    #[inline]
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        unsafe { Arc::get_mut_unchecked(&mut self.arc) }
-    }
-}
-
-impl<T> PartialEq for Garc<T> {
-    #[inline]
-    fn eq(&self, other: &Self) -> bool {
-        Arc::as_ptr(&self.arc) == Arc::as_ptr(&other.arc)
-    }
-}
-
-impl<T> Eq for Garc<T> {}
-
-impl<T> Hash for Garc<T> {
-    #[inline]
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        Arc::as_ptr(&self.arc).hash(state);
-    }
-}
-
-unsafe impl<T> Sync for Garc<T> {}
-
-impl<T> Clone for Garc<T> {
-    fn clone(&self) -> Self {
-        Self {
-            arc: self.arc.clone(),
+            #[inline]
+            fn deref(&self) -> &Self::Target {
+                self.inner.deref()
+            }
         }
-    }
+
+        impl<T> DerefMut for $wrapper<T> {
+            #[inline]
+            fn deref_mut(&mut self) -> &mut Self::Target {
+                unsafe { $inner::get_mut_unchecked(&mut self.inner) }
+            }
+        }
+
+        impl<T> PartialEq for $wrapper<T> {
+            #[inline]
+            fn eq(&self, other: &Self) -> bool {
+                $inner::as_ptr(&self.inner) == $inner::as_ptr(&other.inner)
+            }
+        }
+
+        impl<T> Eq for $wrapper<T> {}
+
+        impl<T> Hash for $wrapper<T> {
+            #[inline]
+            fn hash<H: Hasher>(&self, state: &mut H) {
+                $inner::as_ptr(&self.inner).hash(state);
+            }
+        }
+
+        unsafe impl<T> Sync for $wrapper<T> {}
+
+        impl<T> Clone for $wrapper<T> {
+            #[inline]
+            fn clone(&self) -> Self {
+                Self {
+                    inner: self.inner.clone(),
+                }
+            }
+        }
+    };
 }
+
+define_rc_wrapper!(Grc, Rc);
+define_rc_wrapper!(Garc, Arc);
